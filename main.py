ba5d15fa36cf16818cfb91f13fa6fdcd8700721a
Mud <mudkippzs+heroku@gmail.com>
2019-10-31 23:41:14 +0000

    Added motd() and text.

--- main.py
+++ main.py
@@ -4,19 +4,31 @@ import time
 
 SC_Scanner = scanner.Scanner()	
 
+def motd():
+	print("""
+===============================================
+STAR CITIZEN - PLEDGE STORE MONITOR (PSM) v1.0a
+===============================================
+				(@uthor: github.com/mudkippzs)
+
+usage: python main.py
+Future updates will have cli options for alerts, timer setting etc.
+		""")
+
 def main():
 	SC_Scanner.set("url", "https://robertsspaceindustries.com/pledge/extras?product_id=72")
-	while True:
-		now = time.ctime()
-		print("\nShip Price Update (%s)" % (now))
+	while True:		
 		update()
 		sys.stdout.flush()
-		time.sleep(60)		
-		print("\n\n\n")
+		time.sleep(86400)				
 
 def update():
+	now = time.ctime()
+	print("\nShip Price Update (%s)" % (now))		
 	SC_Scanner.print_ship_list()
+	print("\n\n\n")
 
 
 if __name__ == '__main__':
+	motd()
 	main()
